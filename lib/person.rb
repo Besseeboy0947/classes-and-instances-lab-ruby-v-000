56c0f5e9568e0ede68f19d4fe5a8c451f26989d8
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-06-05 00:16:28 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
-class person
+class Person
 end
  
-alan_kay  = person.new
+alan_kay  = Person.new
 alan_kay #=> #<Dog:0x007fc52c2d7d20>
  
-adele_goldberg = person.new
+adele_goldberg = Person.new
 adele_goldberg #=> #<person:0x003fc52c2d4170>
  
 
