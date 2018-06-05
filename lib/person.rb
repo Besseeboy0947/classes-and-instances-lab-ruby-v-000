a0de4bc2dbae96aeddf917ad622e9abd3481135c
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-06-05 00:12:42 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,5 +7,5 @@ robo #=> #<Dog:0x007fc52c2d7d20>
 namelessG = person.new
 namelessG #=> #<person:0x003fc52c2d4170>
  
-lassie = person.new
-lassie #=> #<person:0x007fc52c2cc588>
+folly = person.new
+folly #=> #<person:0x007fc52c2cc588>
