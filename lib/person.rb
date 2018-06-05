13803a045019ff5a4472af1c4845f379650c792b
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-06-05 00:11:21 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
-class r
+class person
 end
  
-fido = Dog.new
-fido #=> #<Dog:0x007fc52c2d7d20>
+robo = person.new
+robo #=> #<Dog:0x007fc52c2d7d20>
  
-snoopy = Dog.new
-snoopy #=> #<Dog:0x007fc52c2d4170>
+namelessG = Dog.new
+namelessG #=> #<person:0x003fc52c2d4170>
  
 lassie = Dog.new
 lassie #=> #<Dog:0x007fc52c2cc588>
